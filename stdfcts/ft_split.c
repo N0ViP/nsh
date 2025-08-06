2dc1b1dc0a59096b463b1e38b1853ae8152c6cf5
N0ViP <01.xx.vip@gmail.com>
2025-08-06 02:45:30 +0100

    refactor(stdfcts): align ft_strchr with libc version and update usage
    - Remove the compiled binary `a.out` as it should not be tracked by version control.
    - Adapt `ft_split` by introducing a `check_if_match` helper to handle the modified return type of `ft_strchr`.
    - Modify `ft_strchr` to align with the standard C library, changing the return type from `bool` to `char *` to point to the found character.

--- stdfcts/ft_split.c
+++ stdfcts/ft_split.c
@@ -3,12 +3,21 @@
 #define COUNT_SPACES	true
 #define COUNT_WORD		false
 
+static bool	check_if_match(char *sep, char c)
+{
+	if (ft_strchr(sep, c))
+	{
+		return (true);
+	}
+	return (false);
+}
+
 static size_t	count_chars(const char *s, const char *sep, bool flag)
 {
 	size_t	i;
 
 	i = 0;
-	while (s[i] && ft_strchr(sep, s[i]) == flag)
+	while (s[i] && check_if_match(sep, s[i]) == flag)
 	{
 		i++;
 	}
--- stdfcts/ft_strchr.c
+++ stdfcts/ft_strchr.c
@@ -6,22 +6,22 @@
 /*   By: yjaafar <yjaafar@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 09:48:40 by yjaafar           #+#    #+#             */
-/*   Updated: 2025/08/06 01:49:44 by yjaafar          ###   ########.fr       */
+/*   Updated: 2025/08/06 02:04:25 by yjaafar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
-bool	ft_strchr(const char *str, char c)
+char	*ft_strchr(const char *str, char c)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
 	while (str[i])
 	{
 		if (c == str[i])
-			return (true);
+			return ((char *) &str[i]);
 		i++;
 	}
-	return (false);
+	return (NULL);
 }
