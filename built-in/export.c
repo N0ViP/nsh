5d2977e9cae118186e9c3a64d71af11ab96b1163
N0ViP <01.xx.vip@gmail.com>
2025-08-10 16:49:04 +0100

    feat(export): add identifier validation and error handling
    - built-in/export.c: Implement identifier validation for the `export` command by adding `check_name` and `syntax_error` static functions, and update `built_in_export` to return an exit status of 1 on failure.

--- built-in/export.c
+++ built-in/export.c
@@ -1,10 +1,45 @@
 #include "built-in.h"
 
-void	export(char **args)
+static void	syntax_error(char *str)
+{
+	char	*arr[4];
+	char	*message;
+
+	arr[0] = "nsh: export: `";
+	arr[1] = str;
+	arr[2] = "': not a valid identifier\n";
+	arr[3] = NULL;
+	message = ft_strjoin(arr, "");
+	write(2, message, ft_strlen(message));
+}
+
+static bool	check_name(char *str)
 {
 	size_t	i;
 
 	i = 0;
+	if (!ft_isalpha(str[0]))
+	{
+		return (false);
+	}
+	while (str[i] && str[i] != '=')
+	{
+		if (!ft_isalnum(str[i]))
+		{
+			return (false);
+		}
+		i++;
+	}
+	return (true);
+}
+
+int	built_in_export(char **args)
+{
+	size_t	i;
+	int		exit_status;
+
+	i = 0;
+	exit_status = 0;
 	if (!args[1])
 	{
 		print_env();
@@ -17,7 +52,9 @@ void	export(char **args)
 		}
 		else
 		{
-			;
+			syntax_error(args[1]);
+			exit_status = 1;
 		}
 	}
+	return (exit_status);
 }
