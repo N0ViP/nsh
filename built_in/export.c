b13a928b18a42849b1536311eadbbae5095866b4
N0ViP <01.xx.vip@gmail.com>
2025-08-11 11:50:31 +0100

    feat(export): implement 'declare -x' output and process multiple arguments
    - In `built_in/export.c`, refactor the `built_in_export` function to support multiple variable assignments in a single command and implement `export -p` behavior by modifying the `print_env` helper to format output with `declare -x`.

--- built_in/export.c
+++ built_in/export.c
@@ -33,12 +33,29 @@ static bool	check_name(char *str)
 	return (true);
 }
 
-static void	print_env(void)
+static int	print_env(void)
 {
 	t_list_info	*env;
+	t_list		*ptr;
+	char		*start;
+	char		*equal;
 
 	env = ft_getenv(GET_ENV, NULL);
-	
+	ptr = env->list;
+	while (ptr)
+	{
+		start = (char *)ptr->content;
+		equal = ft_strchr(ptr->content, '=');
+		if (equal)
+		{
+			printf("declare -x %.*s\"%s\"\n", (int)(equal - start + 1), ptr->content, ptr->content + (equal - start) + 1);
+		}
+		else
+		{
+			printf("declare -x %s\n", (char *)ptr->content);
+		}
+		ptr = ptr->next;
+	}
 }
 
 int	built_in_export(char **args)
@@ -46,23 +63,25 @@ int	built_in_export(char **args)
 	size_t	i;
 	int		exit_status;
 
-	i = 0;
+	i = 1;
 	exit_status = 0;
 	if (!args[1])
 	{
-		print_env();
+		exit_status = print_env();
+		return (exit_status);
 	}
-	else
+	while (args[i])
 	{
-		if (check_name(args[1]))
+		if (check_name(args[i]))
 		{
-			ft_getenv(ADD_IN_ENV, args[1]);
+			ft_getenv(ADD_IN_ENV, args[i]);
 		}
 		else
 		{
-			syntax_error(args[1]);
+			syntax_error(args[i]);
 			exit_status = 1;
 		}
+		i++;
 	}
 	return (exit_status);
 }
